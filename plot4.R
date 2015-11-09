01803ae0b562ba874faf3fb1a9ac6980d7b4b86e
Matej Bacik <matejbacik@gmail.com>
2015-11-09 01:20:08 +0100

    removing box of legend form plot4

--- plot4.R
+++ plot4.R
@@ -25,7 +25,7 @@ draw_plot4 <- function(data = NULL){
     plot(datetime, data$Sub_metering_1, type = 'l', xlab = '', ylab = 'Energy sub metering')
     lines(datetime, data$Sub_metering_2, col = 'red')
     lines(datetime, data$Sub_metering_3, col = 'blue')
-    legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=c(1,1,1), col = c('black', 'red', 'blue'))
+    legend('topright', bty = "n", legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=c(1,1,1), col = c('black', 'red', 'blue'))
     
     # toprigth plot
     with(data, plot(datetime, Voltage, type = 'l'))
